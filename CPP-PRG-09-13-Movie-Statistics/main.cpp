fe9d6662630f9fb8d9b04b9d8a1ef0008f075495
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-31 21:00:59 -0400

    Prompt written.

--- CPP-PRG-09-13-Movie-Statistics/main.cpp
+++ CPP-PRG-09-13-Movie-Statistics/main.cpp
@@ -5,11 +5,23 @@
 //  Created by Keith Smith on 10/31/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that can be used to gather statistical data about the number of movies
+//  college students see in a month. The program should perform the following steps:
+//
+//      A)  Ask the user how many students were surveyed. An array of integers with this many
+//          elements should then be dynamically allocated.
+//      B)  Allow the user to enter the number of movies each student saw into the array.
+//      C)  Calculate and display the average, median, and mode of the values entered. (Use the
+//          functions you wrote in Programming Challenges 8 and 9 to calculate the median and
+//          mode.)
+//
+//  Input Validation: Do not accept negative numbers for input.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
