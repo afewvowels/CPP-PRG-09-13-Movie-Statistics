19102f842d79c459912525eb1ddd211f8eae460c
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-31 20:57:59 -0400

    Initial Commit

---
+++ CPP-PRG-09-13-Movie-Statistics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-PRG-09-13-Movie-Statistics
+//
+//  Created by Keith Smith on 10/31/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
